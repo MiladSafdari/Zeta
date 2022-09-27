50fc325c527c2bd111fa6554f6c57a0133362a8d
Milad <milad.fx118@gmail.com>
2022-09-27 21:18:08 +0200

    899

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ if(window.screen.width < 1024){
     window.alert("this is Disktop Site");
     window.location.replace("./error.html");git 
 }
+function websiteVisits(response){
+    document.querySelector("#visits").textContent=response.value;
+}
